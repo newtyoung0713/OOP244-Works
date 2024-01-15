171f8685b56a1590dfcc3fc75c7fefb0aaae2219
Yang <yang@yangdembp.home>
2024-01-15 00:58:37 -0500

    Updating assignment v1

---
+++ WS01/lab/main.cpp
@@ -0,0 +1,67 @@
+/***********************************************************************
+Workshop 1: Compiling modular source code
+File	w1p2.cpp
+Version 1.0
+Author	Sheng-Lin Yang  2241
+Description:
+   This provides some source code in a single file to break 
+   into modules and compile together
+
+Revision History
+-----------------------------------------------------------
+Name            Date            Reason       
+
+***********************************************************************/
+
+
+#include <iostream>
+#include "graph.h"
+#include "io.h"
+using namespace seneca;
+using namespace std;
+
+void samplesFirst();
+
+// SeneGraph program
+int main() {
+  int noOfSamples = 0;
+  int samples[MAX_NO_OF_SAMPLES] = {0};
+  bool done = false;
+  labelLine(33, "Welcome to SeneGraph");
+  while (!done) {
+    switch (menu(noOfSamples)) {
+    case 1:
+      cout << "Enter number of available samples: ";
+      noOfSamples = getInt(1, MAX_NO_OF_SAMPLES);
+      break;
+    case 2:
+      if (noOfSamples == 0) {
+        samplesFirst();
+      }
+      else {
+        cout << "Please enter the sample values: " << endl;
+        getSamples(samples, noOfSamples);
+      }
+      break;
+    case 3:
+      if (noOfSamples == 0) {
+        samplesFirst();
+      }
+      else if (samples[0] == 0) {
+        cout << "Samples missing. Please enter the sample values!" << endl;
+      }
+      else {
+        printGraph(samples, noOfSamples, " SeneGraph ");
+      }
+      break;
+    case 0:
+      cout << "Thanks for using SeneGraph" << endl;
+      done = true;
+    }
+  }
+  return 0;
+}
+// prints samples error message
+void samplesFirst() {
+  cout << "Number of Samples must be set first!" << endl;
+}
