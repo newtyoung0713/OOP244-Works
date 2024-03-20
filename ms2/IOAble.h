a17e20444f627e6e55d65cd1f461848b37935e61
Yang <yang@yangdembp.home>
2024-03-20 19:15:35 -0400

    Completed ms2

---
+++ ms2/IOAble.h
@@ -0,0 +1,47 @@
+//*****************************************************************************
+//  <assessment name example : Milestone - #2>
+//  Full Name   : Sheng-Lin Yang
+//  Student ID# : 160443222
+//	Email       : syang153@myseneca.ca
+//	Section     : OOP244 NAA
+//	Date        : March 16th, 2024
+//	Authenticity Declaration :
+//  I have done all the coding by myself and only copied the code 
+//  that my professor provided to complete my milestones and assignments.
+//*****************************************************************************
+#ifndef SENECA_IOABLE_H
+#define SENECA_IOABLE_H
+#include <iostream>
+namespace seneca {
+  class IOAble {
+  public:
+    /* 
+    This pure virtual function is for future ostream outputs. 
+    It receives a reference of an ostream and returns a reference of an ostream. 
+    This function is incapable of changing the current object. 
+    */
+    virtual std::ostream& write(std::ostream& os) const = 0;
+
+    /* 
+    This pure virtual function is for future istream inputs. 
+    It receives a reference of istream and returns a reference of an istream. 
+    */
+    virtual std::istream& read(std::istream& is) = 0;
+
+    // This class also has an empty virtual destructor.
+    virtual ~IOAble() = default;
+
+    /* 
+    Overload the insertion operator to be able to insert the information 
+    of an IOAble object into ostream using the IOAble::write function. 
+    */
+    friend std::ostream& operator<<(std::ostream& os, const IOAble& ioa);
+
+    /* 
+    Overload the extraction operator to be able to extract information 
+    from an istream into an IOAble object using the IOAble::read function. 
+    */
+    friend std::istream& operator>>(std::istream& is, IOAble& ioa);
+  };
+}
+#endif  //  !SENECA_IOABLE_H 
